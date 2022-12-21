c12e6b0d52e1c6b8c3cdcb921c559a173f59447e
Hasnain <mrajhasnain101@gmail.com>
2022-12-22 01:48:50 +0600

    user data fetched

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React from 'react';
 
 import Search from './components/Search';
 import Users from './components/Users';
+import useFetch from './hook/useFetch';
 
 const App = () => {
+  const {users,isLoading,error}=useFetch('https://jsonplaceholder.typicode.com/users')
   // Task 2: use custom hook
   // get data, error, isLoading states from custom hook here
   // use url: 'https://jsonplaceholder.typicode.com/users'
-
+  // const=useFetch('https://jsonplaceholder.typicode.com/users')
   // Task 3: delete user
   // get the id from User.js
   const handleDeleteUser = (id) => {};
 
   // Task 4: search user
   // get the text from Search.js
-  const handleSearch = (searchText) => {};
+  // const handleSearch = (searchText) => {};
 
   return (
     <div className="container">
@@ -23,9 +25,9 @@ const App = () => {
       {isLoading && <p>Loading users...</p>}
       {error && <p>{error}</p>}
 
-      Needs to pass functions from here for state lifting 
-      <Search onHandleSearch={} />
-      {users.length > 1 && <Users users={usersCopy} onHandleDeleteUser={} />}
+      {/* Needs to pass functions from here for state lifting  */}
+      {/* <Search onHandleSearch={} /> */}
+      {users && <Users users={users} onHandleDeleteUser={handleDeleteUser} />}
     </div>
   );
 };
--- src/hook/useFetch.js
+++ src/hook/useFetch.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 
 const useFetch = (url) => {
   // Task 1: complete this custom hook
-  // step1: create 3 states: data, isLoading, error
+  // step1: create 3 states: setUsers, isLoading, error
   // step2: fetch data & handle error
   // step3: return 3 states
+  const [users,setUsers]=useState([])
+  const [isLoading,setisLoading]=useState(true)
+  const [error,setError]=useState(null)
+
+  useEffect(() => {
+    fetch(url)
+    .then((res)=>{
+      setisLoading(false)
+      if(!res.ok){
+        throw new Error('data fetch error')
+      }
+      return res.json()
+    })
+    .then((data)=>{
+      setUsers(data)
+    })
+    .catch((error)=>{
+      setError(error.message)
+      setisLoading(false)
+    })
+
+  }, []);
+
+  return {users,isLoading,error}
+
 };
 
 export default useFetch;
