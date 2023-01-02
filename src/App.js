0338df5249f77d5c2258e5f1b781f5ee37e925d3
sagor995 <sagorahamed995@gmail.com>
2023-01-02 21:12:54 +0600

    Assignment 5 done.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,48 @@
-import React from 'react';
-
+// import Search from './components/Search';
+import { useEffect, useState } from 'react';
 import Search from './components/Search';
 import Users from './components/Users';
+import useFetch from './hook/useFetch';
 
 const App = () => {
   // Task 2: use custom hook
   // get data, error, isLoading states from custom hook here
   // use url: 'https://jsonplaceholder.typicode.com/users'
 
+  const { isLoading, error, users } = useFetch('https://jsonplaceholder.typicode.com/users');
+  const [usersCopy, setUsersCopy] = useState(users);
+
+  useEffect(() => {
+    setUsersCopy(users);
+  }, [users]);
+
   // Task 3: delete user
   // get the id from User.js
-  const handleDeleteUser = (id) => {};
+  const handleDeleteUser = (id) => {
+    const filter = usersCopy.filter((user) => user.id !== id);
+    setUsersCopy(filter);
+  };
 
   // Task 4: search user
   // get the text from Search.js
-  const handleSearch = (searchText) => {};
+  const handleSearch = (searchText) => {
+    let value = searchText.toLowerCase();
+    const newUsers = users.filter((user) => {
+      const uname = user.name.toLowerCase();
+      return uname.startsWith(value);
+    });
+    setUsersCopy(newUsers);
+  };
 
   return (
     <div className="container">
       <h1 className="title">Users Management App</h1>
       {isLoading && <p>Loading users...</p>}
       {error && <p>{error}</p>}
-
       {/* Needs to pass functions from here for state lifting  */}
-      <Search onHandleSearch={} />
-      {users.length > 1 && <Users users={usersCopy} onHandleDeleteUser={} />}
+      {<Search onHandleSearch={handleSearch} />}
+
+      {users.length > 1 && <Users users={usersCopy} onHandleDeleteUser={handleDeleteUser} />}
     </div>
   );
 };
--- src/hook/useFetch.js
+++ src/hook/useFetch.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 const useFetch = (url) => {
   // Task 1: complete this custom hook
   // step1: create 3 states: data, isLoading, error
   // step2: fetch data & handle error
   // step3: return 3 states
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [users, setUsers] = useState([]);
+
+  // const fetchData = async (url) => {
+  //   setIsLoading(true);
+  //   try {
+  //     const response = await fetch(url);
+  //     const data = await response.json();
+  //     setUsers(data);
+  //     setIsLoading(false);
+  //     setError(null);
+  //   } catch (error) {
+  //     setError(error);
+  //     setIsLoading(false);
+  //   }
+  // };
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error('Something is wrong');
+        } else {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        setUsers(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoading(false);
+      });
+  }, [url]);
+
+  return { isLoading, error, users };
 };
 
 export default useFetch;
