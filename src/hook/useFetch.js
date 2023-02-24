b62be2eaf74c3d8f80cf50a6b7b66f95a455ecd9
tufanmulla <iamtufanmulla@gmail.com>
2023-02-25 02:02:10 +0530

    reac-assignment-5completed

--- src/hook/useFetch.js
+++ src/hook/useFetch.js
@@ -1,10 +1,42 @@
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
 
 const useFetch = (url) => {
   // Task 1: complete this custom hook
   // step1: create 3 states: data, isLoading, error
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+
   // step2: fetch data & handle error
+     useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const res = await fetch(url);
+        const users = await res.json();
+        setUsers(users);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+
+    return fetchData;
+  }, []);
+
+
   // step3: return 3 states
+  return {
+    users,
+    isLoading,
+    error
+  };
 };
+
 
 export default useFetch;
