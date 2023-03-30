8032c02b8d62962b45532604f614610db6c478a0
Sonjoy-K-Sarker <sonjoy.sarker.it.19@gmail.com>
2023-03-30 20:24:06 +0600

    user delete funtionality added

--- src/hook/useFetch.js
+++ src/hook/useFetch.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const useFetch = (url) => {
   // Task 1: complete this custom hook
+
   // step1: create 3 states: data, isLoading, error
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   // step2: fetch data & handle error
+  const dataFetch = async (url) => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(url);
+      if (!res.ok) {
+        throw Error('Data fetching is not completed perfectly.');
+      } else {
+        const data = await res.json();
+        setIsLoading(false);
+        setData(data);
+        setError(null);
+      }
+    } catch (err) {
+      setError(err.message);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    dataFetch(url);
+  }, []);
+
   // step3: return 3 states
+  return { data, isLoading, error };
 };
 
 export default useFetch;
