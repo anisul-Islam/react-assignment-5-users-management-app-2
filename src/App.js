220feae0389d393d33019a71799b83e7012e30f8
rahat-md <rahat013166@gmail.com>
2023-09-25 10:10:28 +0600

    completed task

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
-import React from 'react';
+/* eslint-disable prettier/prettier */
+import React, { useEffect, useState } from 'react';
 
 import Search from './components/Search';
 import Users from './components/Users';
+import useFetch from './hook/useFetch';
 
 const App = () => {
   // Task 2: use custom hook
   // get data, error, isLoading states from custom hook here
   // use url: 'https://jsonplaceholder.typicode.com/users'
-
+  const { data, isLoading, error } = useFetch('https://jsonplaceholder.typicode.com/users');
+  const [usersCopy, setUsersCopy] = useState(data);
+  useEffect(() => {
+    setUsersCopy(data);
+  }, [data]);
   // Task 3: delete user
   // get the id from User.js
-  const handleDeleteUser = (id) => {};
+  const handleDeleteUser = (id) => {
+    const filteredUser = data.filter((user) => user.id !== id);
+    setUsersCopy(filteredUser);
+  };
 
   // Task 4: search user
   // get the text from Search.js
-  const handleSearch = (searchText) => {};
+  const handleSearch = (searchText) => {
+    const value = searchText.toLowerCase();
+    const searchUser = data.filter((user) => {
+      const newUser = user.name.toLowerCase();
+      return newUser.startsWith(value);
+    });
+
+    setUsersCopy(searchUser);
+  };
 
   return (
     <div className="container">
       <h1 className="title">Users Management App</h1>
       {isLoading && <p>Loading users...</p>}
-      {error && <p>{error}</p>}
+      {error && <p>{error.message}</p>}
 
       {/* Needs to pass functions from here for state lifting  */}
-      <Search onHandleSearch={} />
-      {users.length > 1 && <Users users={usersCopy} onHandleDeleteUser={} />}
+      <Search onHandleSearch={handleSearch} />
+      {data.length > 1 && <Users users={usersCopy} onHandleDeleteUser={handleDeleteUser} />}
     </div>
   );
 };
